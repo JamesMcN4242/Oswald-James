7441f4b4a1fdfb2c40ff5e7c7d9e5f1e69158376
Oswald-Maximus <32834939+Oswald-Maximus@users.noreply.github.com>
2020-06-11 22:17:44 +0100

    add equipable scriptableobject

---
+++ Assets/WorkingTitle/Scripts/ScriptableObjects/EquipableData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EquipableData", menuName = "ScriptableData/EquipableData", order = 0)]
+public class EquipableData : ScriptableObject
+{
+    public string m_className = null;
+    public int m_health = 0;
+    public float m_accuracy = 0.0f;
+    public float m_attackDamage = 0.0f;
+    public float m_defence = 0.0f;
+    public float m_speed = 0.0f;
+}
