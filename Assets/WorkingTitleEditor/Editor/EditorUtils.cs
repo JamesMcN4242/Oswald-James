04b55aa3f06b3cd370b8d7d7176dca56c80ebe0b
Oswald-Maximus <32834939+Oswald-Maximus@users.noreply.github.com>
2020-06-11 23:01:42 +0100

    add popup dialog

--- Assets/WorkingTitleEditor/Editor/EditorUtils.cs
+++ Assets/WorkingTitleEditor/Editor/EditorUtils.cs
@@ -9,5 +9,25 @@ public static class EditorUtils
     public static void PullCharacterData()
     {
         bool succeeded = CreateScriptableObjects<CharacterData>("CharacterData", Path.Combine("Assets", "WorkingTitle", "Resources", "CharacterData", "{0}.asset"));
+        SuccessMessage(succeeded);
+    }
+
+    [MenuItem("Data/Equipable")]
+    public static void PullEquipableData()
+    {
+        bool succeeded = CreateScriptableObjects<EquipableData>("EquipableData", Path.Combine("Assets", "WorkingTitle", "Resources", "EquipableData", "{0}.asset"));
+        SuccessMessage(succeeded);
+    }
+
+    public static void SuccessMessage(bool succeeded)
+    {
+        if (succeeded)
+        {
+            bool message = EditorUtility.DisplayDialog("Data pull success", "Successfully pulled data", "OK");
+        }
+        else
+        {
+            bool message = EditorUtility.DisplayDialog("Data pull failure", "Failed to pull data", "OK");
+        }
     }
 }
