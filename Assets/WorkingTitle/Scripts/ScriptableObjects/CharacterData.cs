35ff06a80a13de3f2c942e45bf63187107ffe1a1
JamesMcN4242 <jamesmcneil42@hotmail.co.uk>
2020-06-11 22:11:34 +0100

    Character Data scriptable object

---
+++ Assets/WorkingTitle/Scripts/ScriptableObjects/CharacterData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableData/CharacterData", order = 0)]
+public class CharacterData : ScriptableObject
+{
+    public string m_className = null;
+    public int m_health = 0;
+    public float m_accuracy = 0.0f;
+    public float m_attackDamage = 0.0f;
+    public float m_defence = 0.0f;
+    public float m_speed = 0.0f;
+}
