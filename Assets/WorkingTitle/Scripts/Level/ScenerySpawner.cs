9b96748cb2a57927a8037bf3e97bab477adfe1a1
Oswald-Maximus <32834939+Oswald-Maximus@users.noreply.github.com>
2020-06-21 21:11:25 +0100

    add alterations to tile mesh dependant on tile type

--- Assets/WorkingTitle/Scripts/Level/ScenerySpawner.cs
+++ Assets/WorkingTitle/Scripts/Level/ScenerySpawner.cs
@@ -15,42 +15,21 @@ public class ScenerySpawner
     public static void SpawnScenery(Tile[,] gridArray, GridSpawnerSettings gridSpawnerSettings)
     {
         List<Unity.Mathematics.int2> generationCoords = new List<Unity.Mathematics.int2>();
-        const float chanceOfNoWall = 0.9f;
+        
         for (int i = 0; i < gridSpawnerSettings.m_x; ++i)
         {
-            gridArray[i,0].m_isPassable = false;
-            gridArray[i,0].m_tileType = Tile.TileType.WALL;
+            BoundaryWall(gridArray, i, 0, generationCoords);
 
-            if (Random.value > chanceOfNoWall)
-            {
-                generationCoords.Add(new Unity.Mathematics.int2(i,0));
-            }
-
-            gridArray[i,(gridSpawnerSettings.m_y - 1)].m_isPassable = false;
-            gridArray[i,(gridSpawnerSettings.m_y - 1)].m_tileType = Tile.TileType.WALL;
-
-            if (Random.value > chanceOfNoWall)
-            {
-                generationCoords.Add(new Unity.Mathematics.int2(i,(gridSpawnerSettings.m_y - 1)));
-            }
+            BoundaryWall(gridArray, i, (gridSpawnerSettings.m_y - 1), generationCoords);
+            
         }
         for (int i = 0; i < gridSpawnerSettings.m_y; ++i)
         {
-            gridArray[0,i].m_isPassable = false;
-            gridArray[0,i].m_tileType = Tile.TileType.WALL;
 
-            if (Random.value > chanceOfNoWall)
-            {
-                generationCoords.Add(new Unity.Mathematics.int2(0,i));
-            }
+            BoundaryWall(gridArray, 0, i, generationCoords);
 
-            gridArray[(gridSpawnerSettings.m_x - 1),i].m_isPassable = false;
-            gridArray[(gridSpawnerSettings.m_x - 1),i].m_tileType = Tile.TileType.WALL;
+            BoundaryWall(gridArray, (gridSpawnerSettings.m_x - 1), i, generationCoords);
 
-            if (Random.value > chanceOfNoWall)
-            {
-                generationCoords.Add(new Unity.Mathematics.int2((gridSpawnerSettings.m_x - 1),i));
-            }
         }
         foreach (Unity.Mathematics.int2 currentCoords in generationCoords)
         {
@@ -59,10 +38,37 @@ public class ScenerySpawner
 
     }
 
+    private static void BoundaryWall(Tile[,] gridArray, int x, int y, List<Unity.Mathematics.int2> generationCoords)
+    {
+        if (gridArray[x,y].m_tileType == Tile.TileType.WALL)
+        {
+            return;
+        }
+        const float chanceOfNoWall = 0.9f;
+        Mesh mesh = gridArray[x,y].GetComponent<MeshFilter>().mesh;
+        List<Vector3> vertices = new List<Vector3>();
+        mesh.GetVertices(vertices);
+
+        for (int n = 0; n < vertices.Count; ++n)
+        {
+            vertices[n] = new Vector3(vertices[n].x, vertices[n].y * 4, vertices[n].z);
+        }
+        mesh.SetVertices(vertices, 0, vertices.Count);
+
+        gridArray[x,y].m_isPassable = false;
+        gridArray[x,y].m_tileType = Tile.TileType.WALL;
+
+        if (Random.value > chanceOfNoWall)
+        {
+            generationCoords.Add(new Unity.Mathematics.int2(x,y));
+        }
+
+    }
+
     private static void GenerateWall(Tile[,] gridArray, int x, int y)
     {
         Direction direction = Direction.UP;
-        const float increment = 0.05f;
+        const float increment = 0.03f;
         const float oddOrEven = 0.02f;
         const float halfwallToggleThreshold = 0.9f;
         bool halfwallToggle = false;
@@ -108,14 +114,34 @@ public class ScenerySpawner
                     break;
             }
 
-            if (gridArray[x,y].m_tileType == Tile.TileType.WALL)
+            if (gridArray[x,y].m_tileType != Tile.TileType.FLOOR)
             {
                 isComplete = true;
             }
             else
             {
-                if (halfwallToggle) gridArray[x,y].m_tileType = Tile.TileType.HALFWALL;
-                else gridArray[x,y].m_tileType = Tile.TileType.WALL;
+                Mesh mesh = gridArray[x,y].GetComponent<MeshFilter>().mesh;
+                List<Vector3> vertices = new List<Vector3>();
+                mesh.GetVertices(vertices);
+                
+                if (halfwallToggle)
+                { 
+                    gridArray[x,y].m_tileType = Tile.TileType.HALFWALL;
+                    for (int n = 0; n < vertices.Count; ++n)
+                    {
+                        vertices[n] = new Vector3(vertices[n].x, vertices[n].y * 2, vertices[n].z);
+                    }
+                    mesh.SetVertices(vertices, 0, vertices.Count);        
+                }
+                else 
+                {
+                    gridArray[x,y].m_tileType = Tile.TileType.WALL;
+                    for (int n = 0; n < vertices.Count; ++n)
+                    {
+                        vertices[n] = new Vector3(vertices[n].x, vertices[n].y * 4, vertices[n].z);
+                    }
+                    mesh.SetVertices(vertices, 0, vertices.Count);
+                }
                 gridArray[x,y].m_isPassable = false;
 
                 if (changeDirection > Random.value)
@@ -140,6 +166,19 @@ public class ScenerySpawner
                     gridArray[x,y].m_tileType = Tile.TileType.DOOR;
                     gridArray[x,y].m_isPassable = true;
                     doorChance = 0.00f;
+
+                    for (int n = 0; n < vertices.Count; ++n)
+                    {
+                        if (halfwallToggle)
+                        {
+                            vertices[n] = new Vector3(vertices[n].x, vertices[n].y / 2, vertices[n].z);
+                        }
+                        else
+                        {
+                            vertices[n] = new Vector3(vertices[n].x, vertices[n].y / 4, vertices[n].z);
+                        }
+                    }
+                    mesh.SetVertices(vertices, 0, vertices.Count);
                 }
                 else
                 {
